3d488e4d708226528f5068eb0560e4c9a71cd3c1
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2025-04-01 20:40:21 -0300

    clase 6

---
+++ Clase-6/script.js
@@ -0,0 +1,25 @@
+//JS
+
+//string numbers boolean
+
+
+//SQL 
+
+//VARCHAR - CHAR -> caracteres alfanumericos 255 caracteres 
+//INT(enteros) - DECIMAL(decimales) -> numeros
+//BOOLEAN -> booleans (true-false)
+//DATE -> fecha
+//TEXT -> textos 
+
+
+//VARCHAR(100)
+
+
+//XAMPP -> programa para generar DB(data base = base de datos) relacionales 
+//MySQLWorkbench -> programa para generar DB relacionales
+
+//MariaDB - MySQL -> tipo de base de datos relacional
+//SQL -> lenguaje que manejan las DB relacionales 
+
+
+//Break!!! 20:55 volvemos
