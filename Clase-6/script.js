90ca1a50613d20b1d7e2a4973b56150e05315dad
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2025-04-01 22:17:31 -0300

    clase 6

--- Clase-6/script.js
+++ Clase-6/script.js
@@ -4,6 +4,9 @@
 
 
 //SQL 
+//informacion en forma de tablas
+//columnas -> campos
+// filas -> registros
 
 //VARCHAR - CHAR -> caracteres alfanumericos 255 caracteres 
 //INT(enteros) - DECIMAL(decimales) -> numeros
@@ -15,11 +18,47 @@
 //VARCHAR(100)
 
 
-//XAMPP -> programa para generar DB(data base = base de datos) relacionales 
+//XAMPP -> programa para generar DB(data base = base de datos) relacionales de manera local
 //MySQLWorkbench -> programa para generar DB relacionales
 
 //MariaDB - MySQL -> tipo de base de datos relacional
 //SQL -> lenguaje que manejan las DB relacionales 
 
 
-//Break!!! 20:55 volvemos
+//-----------------------------
+
+
+//Comandos SQL
+
+//-----
+// Data Definition Lenguaje (DDL)
+// comandos que me permiten manejar-alterar campos-tabla-db  (estructura)
+// el manejo del esqueleto de la DB 
+
+//CREATE -> crear : bases de datos y tablas 
+
+//CREATE DATABASE nombre;
+
+//CREATE TABLE nombreTabla(
+// nombreCampo tipo restriccion,
+// nombreCampo tipo restriccion,
+//nombreCampo tipo restriccion,)
+
+
+//ALTER -> altera la tabla
+//ALTER TABLE tabla ADD COLUMN campo tipo restriccion;
+
+
+//-----
+//Data Manipulation Lenguaje (DML)
+
+//comandos referidos al manejo de los registros (la informacion que agrego a la tabla)
+
+//INSERT -> agregar registros a una tabla
+
+//INSERT INTO tabla(campos) VALUES (registro),(registro);
+
+
+//Proxima clase
+//hablar comando DDL y DML ,ejercicio 
+// relacion entre tablas de una misma DB
